816452b75433ed4c8929a2cddb26a10fcdde8d34
Kojo Idrissa <kojo.idrissa@gmail.com>
2020-06-16 13:41:02 -0500

    Admin improvements

--- talks/admin.py
+++ talks/admin.py
@@ -5,4 +5,17 @@ from .models import Talk
 # https://docs.djangoproject.com/en/3.0/ref/contrib/admin/#modeladmin-objects
 
 
-admin.site.register(Talk)
+class TalkAdmin(admin.ModelAdmin):
+    """The ModelAdmin class is the representation of a model in the admin."""
+
+    list_display = ["talk_title"]
+    search_fields = [
+        "personal_name",
+        "family_name",
+        "talk_title",
+        "contact_email",
+        "twitter_handle",
+    ]
+
+
+admin.site.register(Talk, TalkAdmin)
