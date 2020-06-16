e5bb7043476e6695872b07c3540759cb0a8cb9eb
Kojo Idrissa <kojo.idrissa@gmail.com>
2020-06-16 15:58:17 -0500

    created talk detail view and shim template

--- talks/views.py
+++ talks/views.py
@@ -10,3 +10,10 @@ def talk_list_view(request):
     talk_list = Talk.objects.all()
     context = {"talk_list": talk_list}
     return render(request, "talks/talk_list.html", context)
+
+
+def talk_detail_view(request, id):
+    """Return single talk."""
+    single_talk = Talk.objects.get(id=id)
+    context = {"talk": single_talk, }
+    return render(request, "talks/talk_detail.html", context)
