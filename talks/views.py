42f34859bc2613a6ee2969f22f1b82aef59fd2f8
Kojo Idrissa <kojo.idrissa@gmail.com>
2020-06-16 14:52:30 -0500

    created list_view

--- talks/views.py
+++ talks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+# https://docs.djangoproject.com/en/3.0/topics/http/shortcuts/
 
-# Create your views here.
+from .models import Talk
+
+
+def list_view(request):
+    """Create a list of all talks."""
+    # https://docs.djangoproject.com/en/3.0/topics/db/queries/#retrieving-all-objects
+    talk_list = Talk.objects.all()
+    context = {"talk_list": talk_list}
+    return render(request, "talks/talk_list.html", context)
