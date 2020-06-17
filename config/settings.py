0faba383fda5f40f3238ca740259c311f8cd8ae2
Kojo Idrissa <kojo.idrissa@gmail.com>
2020-06-16 22:02:34 -0500

    tiny tweaks

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jogp$b6fp@txt+3x^$1mki#ajps+_y3(=t&c43mezi422yp$x&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["127.0.0.1"]
